fe74e4290b97180e403a77b07b7e1b80da9e8c59
Kevin Sanchez <kev.sani@hotmail.com>
2021-10-03 07:14:33 -1000

    Converted Jetty server to Tomcat Spring boot.

---
+++ src/main/java/org/example/config/WebAppConfig.java
@@ -0,0 +1,10 @@
+package org.example.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebAppConfig {
+    public WebAppConfig() {
+        // Default Constructor for Spring.
+    }
+}
